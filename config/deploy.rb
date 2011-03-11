a8751cabd4719cf52db7d328a7e83f4d8ee47abb
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-03-11 07:11:26 +0000

    修改了shared的符号链接，使之不与s2.tongshare.com共用一份。另外加了新功能的提示语
    git-svn-id: http://tongshare.googlecode.com/svn/branches/branch_reminder/tongshare@350 c2c84ded-d1d5-93fd-fce7-e2d88d762596

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :scm, :subversion
 set :scm_user, "paullzn"
 set :scm_password, "Zv9Kw9sF2kJ9"
 
-server "s2.tongshare.com", :app, :web, :db, :primary => true
+server "t.tongshare.com", :app, :web, :db, :primary => true
 
 default_run_options[:pty] = true
 
@@ -47,5 +47,7 @@ after "deploy:update_code", :link_and_bundle_install
 desc "install the necessary prerequisites"
 task :link_and_bundle_install, :roles => :app do
 #  run "cd #{release_path} && bundle install"
-  run "ln -s /var/www/tongshare/shared/environments/production.rb #{release_path}/config/environments/production.rb && ln -s /var/www/tongshare/shared/public/javascripts/translations.js #{release_path}/public/javascripts/translations.js && ln -s /var/www/tongshare/shared/data #{release_path}/data && cd #{release_path} && bundle install"
+  run "ln -s /var/www/tongshare_branch/shared/environments/production.rb #{release_path}/config/environments/production.rb && \
+    ln -s /var/www/tongshare_branch/shared/public/javascripts/translations.js #{release_path}/public/javascripts/translations.js && \
+    ln -s /var/www/tongshare_branch/shared/data #{release_path}/data && cd #{release_path} && bundle install"
 end
