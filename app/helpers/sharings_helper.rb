398ee2646abcd43900273213c6bd00dca476c44c
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-03-16 03:03:59 +0000

    增强了解析邀请的适用性，不需要全部匹配，只需要中间有一部分就可以（这样就可以搞定带引号的内容）
    git-svn-id: http://tongshare.googlecode.com/svn/branches/branch_reminder/tongshare@390 c2c84ded-d1d5-93fd-fce7-e2d88d762596

--- app/helpers/sharings_helper.rb
+++ app/helpers/sharings_helper.rb
@@ -12,23 +12,24 @@ module SharingsHelper
     result = []
 
     #TODO: copied from user_identifier. Consider a better code style, like putting them in configurations.
-    employee_no_pattern = /^[0-9]{10}$/  #TODO: should get patterns for different schools
-    mobile_pattern = /^1[0-9]{10}$/
-    email_pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
+    employee_no_pattern = /[0-9]{10}/  #TODO: should get patterns for different schools
+    mobile_pattern = /1[0-9]{10}/
+    email_pattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/
 
     elements = str.split(/(\s|,|;|\n)+/)
     for elem in elements
       next if elem.empty?
-      if elem.match(employee_no_pattern) != nil
+      m = nil
+      if (m = elem.match(employee_no_pattern))
         type = UserIdentifier::TYPE_EMPLOYEE_NO
-      elsif elem.match(email_pattern) != nil
+      elsif (m = elem.match(email_pattern))
         type = UserIdentifier::TYPE_EMAIL
-      elsif elem.match(mobile_pattern) != nil
+      elsif (m = elem.match(mobile_pattern))
         type = UserIdentifier::TYPE_MOBILE
       else
         type = nil
       end
-      result << {:type => type, :login_value => elem}
+      result << {:type => type, :login_value => m ? m[0] : elem}
     end
 
     result
