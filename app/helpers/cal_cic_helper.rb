6ef4da19957c3c595cb5d4e8d3efe8c7356f5633
cbmixx <cbmixx@gmail.com>
2011-04-16 14:34:05 +0800

    remain/destroy

--- app/helpers/cal_cic_helper.rb
+++ app/helpers/cal_cic_helper.rb
@@ -8,26 +8,45 @@ module CalCicHelper
     resp, data = Net::HTTP.post_form(url, post_args)
 
     doc = REXML::Document.new(data)
+    events = []
     doc.elements.each('calendar/event') do |e|
       name = e.get_elements('summary').first.text
       start_time = Time.parse(e.get_elements('startTime').first.text).getlocal
       end_time = Time.parse(e.get_elements('endTime').first.text).getlocal
-      if end_time - start_time == 24 * 60 * 60 && end_time.hour == 0 && end_time.min == 0 && end_time.sec == 0
-        end_time -= 1
-      end
+      # won't need
+      # if end_time - start_time == 24 * 60 * 60 && end_time.hour == 0 && end_time.min == 0 && end_time.sec == 0
+      #   end_time -= 1
+      # end
       location = e.get_elements('location').first.text
       location = '' if location.nil?
       extra_info = e.get_elements('description').first.text
-      v = Event.new(
-        :creator_id => user_id,
-        :begin => start_time,
-        :end => end_time,
-        :name => name,
-        :location => location,
-        :extra_info => extra_info,
-        :rrule_frequency => GCal4Ruby::Recurrence::NONE_FREQUENCY)
-      v.save
+      query = Event.find_all_by_creator_id_and_begin_and_end_and_name_and_location_and_extra_info(
+        user_id, start_time, end_time, name, location, extra_info
+      )
+      if query.size == 0
+        v = Event.new(
+          :creator_id => user_id,
+          :begin => start_time,
+          :end => end_time,
+          :name => name,
+          :location => location,
+          :extra_info => extra_info,
+          :rrule_frequency => GCal4Ruby::Recurrence::NONE_FREQUENCY)
+        v.save
+      else
+        v = query.first
+      end
+      events << v.id
     end
+    a = Event.find_all_by_creator_id user_id
+    a.each do |e|
+      if !events.include?(e.id)
+        puts 'event id = ' + e.id.to_s + ' destroy'
+        e.destroy
+      else
+        puts 'event id = ' + e.id.to_s + ' remain'
+      end
+    end  
   end
 
   def destroy_all_events_of_user(user_id)
